17c7f100dae65430866b3e040516a33007d6bc28
naren <naren@Naidu>
2022-10-08 18:56:10 +0530

    Oct8th build release first commit

---
+++ src/releaseTestScripts/Test1.java
@@ -0,0 +1,12 @@
+package releaseTestScripts;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is Oct8th Build Release test1");
+
+	}
+
+}
